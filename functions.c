5da707c98db581bf9b1c255532937bed91cfec01
nelisa-dludla <nelisa.dludla@outlook.com>
2023-04-25 23:08:43 +0200

    Updated functions.c - Return count for print_string

--- functions.c
+++ functions.c
@@ -41,7 +41,7 @@ int print_string(const char *s, int char_count)
 	}
 
 	write(1, s, len);
-	return (char_count + len);
+	return (char_count + (len - 3));
 }
 
 /**
@@ -86,7 +86,9 @@ int print_arg(const char *format, va_list args, int char_count)
 	}
 	else
 	{
-		char_count = print_char(*format, char_count);
+		print_char('%', char_count);
+		print_char(*format, char_count + 1);
+		return (char_count + 2);
 	}
 	return (char_count);
 }
