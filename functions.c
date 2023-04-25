9571bb32e916ad02cd7ba009057711f8afa3623d
nelisa-dludla <nelisa.dludla@outlook.com>
2023-04-25 20:49:12 +0200

    Updated existing files and added a specifiers file to further breakdown the functions

--- functions.c
+++ functions.c
@@ -24,3 +24,38 @@ int _strlen(const char *s)
 
 	return (count);
 }
+
+/**
+ * print_char - Function prints a character
+ * @c: Character to be printed
+ * @char_count: Current character count
+ * Return: New character count
+ */
+int print_char(const char c, int char_count)
+{
+	write(1, &c, 1);
+	return (char_count + 1);
+}
+
+/**
+ * print_string - Function prints a string
+ * @s: String to be printed
+ * @char_count: Current character count
+ * Return: New character count
+ */
+int print_string(const char *s, int char_count)
+{
+	write(1, s, _strlen(s));
+	return (char_count + _strlen(s));
+}
+
+/**
+ * print_precent - Function prints the precent symbol
+ * @char_count: Current character count
+ * Return: New character count
+ */
+int print_precent(const int char_count)
+{
+	write(1, "%", 1);
+	return (char_count + 1);
+}
